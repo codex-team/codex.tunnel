907034f4fe3fe1330211818b4c2b6595fcef0d0a
N0str <ntpcp@yandex.ru>
2018-12-06 16:40:04 +0300

    Move build files to bin directory

--- client/build.sh
+++ client/build.sh
@@ -1,5 +1,5 @@
-go build -o client-macos .
-env GOOS=linux GOARCH=386 go build -o client-linux-i386 .
-env GOOS=linux GOARCH=386 go build -o client-linux-amd64 .
-env GOOS=windows GOARCH=386 go build -o client-windows-i386.exe .
-env GOOS=windows GOARCH=amd64 go build -o client-windows-amd64.exe .
+go build -o bin/client-macos .
+env GOOS=linux GOARCH=386 go build -o bin/client-linux-i386 .
+env GOOS=linux GOARCH=386 go build -o bin/client-linux-amd64 .
+env GOOS=windows GOARCH=386 go build -o bin/client-windows-i386.exe .
+env GOOS=windows GOARCH=amd64 go build -o bin/client-windows-amd64.exe .
